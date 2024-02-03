dda59ebb548cf5166b222d22589c61599d2e2ba8
dickyanthony <dickyanthony0812@gmail.com>
2024-02-03 21:04:55 +0700

    DA-General: Simplify animateSocialMedia code

--- src/component/AnimateSocialMedia.jsx
+++ src/component/AnimateSocialMedia.jsx
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
 import { Title } from "./Title";
-import { motion } from "framer-motion";
 import {
   PiInstagramLogoFill,
   PiLinkedinLogoFill,
@@ -9,9 +8,11 @@ import {
   PiGithubLogoFill,
   PiYoutubeLogoFill,
 } from "react-icons/pi";
+import "./../style/animateSocialMedia.css";
 
 const AnimateSocialMedia = (props) => {
   const {
+    title = "FOLLOW ME",
     className,
     size = 25,
     showInstagram = false,
@@ -28,125 +29,51 @@ const AnimateSocialMedia = (props) => {
     onClickYoutube = () => {},
   } = props;
 
-  const [hoveredIcon, setHoveredIcon] = useState(null);
-
-  const iconVariants = {
-    hover: { translateY: -3 },
-    unhover: { scale: 1 },
-  };
-
-  const handleIconHover = (icon) => {
-    setHoveredIcon(icon?.type.name);
-  };
-
-  const handleIconUnhover = () => {
-    setHoveredIcon(null);
-  };
-
-  const getIconColor = (icon) => {
-    return hoveredIcon === icon.type.name
-      ? "text-white duration-500 transition transition-colors ease-in-out hover:text-white"
-      : "text-slate-400 duration-500 transition transition-colors ease-in-out hover:text-white";
-  };
   return (
     <div className={`${className} w-full`}>
-      <Title className="text-sm">FOLLOW ME</Title>
-      <div className="flex flex-1 flex-row mt-2 gap-3">
+      <Title className="text-sm">{title}</Title>
+      <div className="flex flex-1 flex-row mt-2 gap-3 containerSocialMedia">
         {showInstagram && (
-          <motion.div
-            variants={iconVariants}
-            whileHover="hover"
-            initial="unhover"
-            className="hover:cursor-pointer"
+          <PiInstagramLogoFill
+            className="item text-sky-100 hover:cursor-pointer"
+            size={size}
             onClick={onClickInstagram}
-            onMouseEnter={() => handleIconHover(<PiInstagramLogoFill />)}
-            onMouseLeave={handleIconUnhover}
-          >
-            <PiInstagramLogoFill
-              className={getIconColor(<PiInstagramLogoFill />)}
-              size={size}
-            />
-          </motion.div>
+          />
         )}
         {showLinkedin && (
-          <motion.div
-            variants={iconVariants}
-            whileHover="hover"
-            initial="unhover"
-            className="hover:cursor-pointer"
+          <PiLinkedinLogoFill
+            className="item text-sky-100 hover:cursor-pointer"
+            size={size}
             onClick={onClickLinkedin}
-            onMouseEnter={() => handleIconHover(<PiLinkedinLogoFill />)}
-            onMouseLeave={handleIconUnhover}
-          >
-            <PiLinkedinLogoFill
-              className={getIconColor(<PiLinkedinLogoFill />)}
-              size={size}
-            />
-          </motion.div>
+          />
         )}
         {showFacebook && (
-          <motion.div
-            variants={iconVariants}
-            whileHover="hover"
-            initial="unhover"
-            className="hover:cursor-pointer"
+          <PiFacebookLogoFill
+            className="item text-sky-100 hover:cursor-pointer"
+            size={size}
             onClick={onClickFacebook}
-            onMouseEnter={() => handleIconHover(<PiFacebookLogoFill />)}
-            onMouseLeave={handleIconUnhover}
-          >
-            <PiFacebookLogoFill
-              className={getIconColor(<PiFacebookLogoFill />)}
-              size={size}
-            />
-          </motion.div>
+          />
         )}
         {showTwitter && (
-          <motion.div
-            variants={iconVariants}
-            whileHover="hover"
-            initial="unhover"
-            className="hover:cursor-pointer"
+          <PiTwitterLogoFill
+            className="item text-sky-100 hover:cursor-pointer"
+            size={size}
             onClick={onClickTwitter}
-            onMouseEnter={() => handleIconHover(<PiTwitterLogoFill />)}
-            onMouseLeave={handleIconUnhover}
-          >
-            <PiTwitterLogoFill
-              className={getIconColor(<PiTwitterLogoFill />)}
-              size={size}
-            />
-          </motion.div>
+          />
         )}
         {showGithub && (
-          <motion.div
-            variants={iconVariants}
-            whileHover="hover"
-            initial="unhover"
-            className="hover:cursor-pointer "
+          <PiGithubLogoFill
+            className="item text-sky-100 hover:cursor-pointer"
+            size={size}
             onClick={onClickGithub}
-            onMouseEnter={() => handleIconHover(<PiGithubLogoFill />)}
-            onMouseLeave={handleIconUnhover}
-          >
-            <PiGithubLogoFill
-              className={getIconColor(<PiGithubLogoFill />)}
-              size={size}
-            />
-          </motion.div>
+          />
         )}
         {showYoutube && (
-          <motion.div
-            variants={iconVariants}
-            whileHover="hover"
-            initial="unhover"
-            className="hover:cursor-pointer "
+          <PiYoutubeLogoFill
+            className="item text-sky-100 hover:cursor-pointer"
+            size={size}
             onClick={onClickYoutube}
-            onMouseEnter={() => handleIconHover(<PiYoutubeLogoFill />)}
-            onMouseLeave={handleIconUnhover}
-          >
-            <PiYoutubeLogoFill
-              className={getIconColor(<PiYoutubeLogoFill />)}
-              size={size}
-            />
-          </motion.div>
+          />
         )}
       </div>
     </div>
